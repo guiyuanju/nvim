ed78135d75ff6d3da507dbdcb83b63682f15a659
guiyuanju <guiyuanjune@outlook.com>
2025-10-11 13:28:24 +0800

    add completion

--- init.lua
+++ init.lua
@@ -395,6 +395,44 @@ require("lazy").setup({
     -- LSP
     { 'neovim/nvim-lspconfig' }, -- no opts, no setup
     { 'mason-org/mason.nvim', opts = {} },
+    -- Completion
+    {
+      'saghen/blink.cmp',
+      -- optional: provides snippets for the snippet source
+      dependencies = {},
+      -- use a release tag to download pre-built binaries
+      version = '1.*',
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = {
+          preset = 'super-tab',
+          ['<C-k>'] = { 'show_documentation' },
+        },
+        appearance = {
+          nerd_font_variant = 'mono'
+        },
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = { documentation = { auto_show = false } },
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+          default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+      },
+      opts_extend = { "sources.default" }
+    },
     -- Color Schemes
     { 'olimorris/onedarkpro.nvim' },
     { 'catppuccin/nvim' },
